a1c52390afb60b15ebbd2eaa505cd679be9b6f5e
ZShaan0 <zishaan.asif@hotmail.co.uk>
2025-03-13 21:32:43 +0000

    adds animated menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 function Header(){
     return (
-        <header className='border-b font-bold p-3'>
-            TestApp
+        <header className='border-b p-3 flex justify-between items-center'>
+            <span className='font-bold'>
+                FirstApp
+            </span>
+
+            <Navigation />
         </header>
     )
 }
